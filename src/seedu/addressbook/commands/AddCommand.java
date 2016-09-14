69381e23007df05d426cb98fec062d110327f566
Antonia Devina <antonia.devina78@gmail.com>
2016-09-14 23:52:44 +0800

    Add Exception to the master class and the AddCommand class

--- src/seedu/addressbook/commands/AddCommand.java
+++ src/seedu/addressbook/commands/AddCommand.java
@@ -58,12 +58,14 @@ public class AddCommand extends Command {
     }
 
     @Override
-    public CommandResult execute() {
+    public CommandResult execute() throws Exception{
         try {
             addressBook.addPerson(toAdd);
             return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
         } catch (UniquePersonList.DuplicatePersonException dpe) {
             return new CommandResult(MESSAGE_DUPLICATE_PERSON);
+        } catch (Exception e) { 
+            throw new Exception(); 
         }
     }
     
